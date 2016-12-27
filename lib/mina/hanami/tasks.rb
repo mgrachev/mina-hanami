6b3d00f93579d91af640ca7ab73690a5d5c1a834
Mikhail Grachev <work@mgrachev.com>
2016-12-27 15:12:23 +0300

    Test commit

--- lib/mina/hanami/tasks.rb
+++ lib/mina/hanami/tasks.rb
@@ -4,6 +4,12 @@ require 'mina/bundler'
 set :hanami_env, 'production'
 set :bundle_prefix, -> { %{HANAMI_ENV="#{fetch(:hanami_env)}" #{fetch(:bundle_bin)} exec} }
 set :hanami, -> { "#{fetch(:bundle_prefix)} hanami" }
+set :asset_dirs, ['apps/*/assets/', 'apps/*/vendor/assets/']
+set :migration_dirs, ['db/migrations']
+set :compiled_asset_path, 'public/assets'
+
+set :shared_files, fetch(:shared_files, []).push('public/assets.json')
+set :shared_dirs, fetch(:shared_dirs, []).push(fetch(:compiled_asset_path))
 
 desc 'Starts an interactive console.'
 task console: :environment do
@@ -14,14 +20,49 @@ end
 namespace :hanami do
   desc 'Migrate database'
   task db_migrate: :environment do
-    hanami_command %w(db migrate), 'Migrating database'
+
+    if fetch(:force_migrate)
+      hanami_command %w(db migrate), 'Migrating database'
+    else
+      command check_for_changes_script(
+                  at: fetch(:migration_dirs),
+                  skip: %{echo "-----> DB migrations unchanged; skipping DB migration"},
+                  changed: %{echo "-----> Migrating database"
+                    #{echo_cmd("#{hanami_command(%w(db migrate))}")}}
+              ), quiet: true
+    end
   end
 
   desc 'Precompiles assets (skips if nothing has changed since the last release).'
   task :assets_precompile do
-    hanami_command %w(assets precompile), 'Precompiling asset files'
+    if fetch(:force_asset_precompile)
+      hanami_command %w(assets precompile), 'Precompiling asset files'
+    else
+      command check_for_changes_script(
+                  at: fetch(:asset_dirs),
+                  skip: %{echo "-----> Skipping asset precompilation"},
+                  changed: %{echo "-----> Precompiling asset files"
+                    #{echo_cmd("#{hanami_command(%w(assets precompile))}")}}
+              ), quiet: true
+    end
   end
 end
+
+def check_for_changes_script(options)
+  diffs = Dir.glob(options[:at]).map do |path|
+    %{diff -qrN "#{fetch(:current_path)}/#{path}" "./#{path}" 2>/dev/null}
+  end.join(' && ')
+
+  %{
+    if #{diffs}
+    then
+      #{options[:skip]}
+    else
+      #{options[:changed]}
+    fi
+  }
+end
+
 
 def hanami_command(tasks = [], description = nil)
   in_path "#{fetch(:current_path)}" do
